f8eb350033cd4f45eafddfc3d2bcdf449090bb85
isadfrn <isadfrn@gmail.com>
2024-01-24 22:01:00 -0300

    feat: Add base test for playwright

---
+++ web-client/tests/app/page.test.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { assert } from "console";
+
+test("Go to /recover_password when click on Esqueceu sua senha", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/");
+  await page.click("text=Esqueceu sua senha?");
+  await page.goto("http://localhost:3000/recover_password");
+});
+
+test("Go to /register when click on Não tem conta?", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.click("text=Não tem conta?");
+  await page.goto("http://localhost:3000/register");
+});
